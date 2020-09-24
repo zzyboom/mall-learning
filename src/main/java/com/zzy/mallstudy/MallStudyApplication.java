132606517fa19b6b2f3d26997e64265931dffafb
zhiyu.zheng <2930289642@qq.com>
2020-09-24 17:16:45 +0800

    test git

---
+++ src/main/java/com/zzy/mallstudy/MallStudyApplication.java
@@ -0,0 +1,14 @@
+package com.zzy.mallstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallStudyApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzy/mallstudy/MallStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.mallstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
